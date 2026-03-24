a4e78af921be0f10abbfdf2f1fbd1a049bcd61c3
niighty-ai <gaeldu81@gmail.com>
2026-03-24 08:29:06 +0100

    Add files via upload

--- scripts/utils.js
+++ scripts/utils.js
@@ -109,5 +109,3 @@ let _fbSaving     = false;
 let _fbInitLoaded = false;
 let _lastSaveTs = 0;
 let jpEditingIdx = null;
-let selectedProjectIds = new Set();  // IDs des projets cochés (affichés dans le Gantt)
-let multiViewMode = false;           // true quand >1 projet sélectionné
