1fc0ef7198b531dda16bebaf3af272c76701bd48
niighty-ai <gaeldu81@gmail.com>
2026-04-09 08:43:11 +0200

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDLpk2xBs-R9gM-tvKZ5abG-EaGNqKUGLk",
   authDomain: "cad-project-manager.firebaseapp.com",
@@ -9,15 +11,22 @@ const firebaseConfig = {
   messagingSenderId: "1005597262191",
   appId: "1:1005597262191:web:86901f88aecbd9ed30689a"
 };
+
 const _fbApp    = initializeApp(firebaseConfig);
 const _fbDb     = getDatabase(_fbApp);
 const _fbRef    = ref(_fbDb, 'gantt_portfolio');
 const _fbResRef = ref(_fbDb, 'gantt_resources');
 const _fbGhoRef = ref(_fbDb, 'gantt_gho');
+const _fbAuth   = getAuth(_fbApp);
 
-window._fbSet             = (data) => set(_fbRef,    data);
-window._fbOnValue         = (cb)   => onValue(_fbRef,    snap => cb(snap.val()));
-window._fbSetResources    = (data) => set(_fbResRef, data);
-window._fbOnValueResources= (cb)   => onValue(_fbResRef, snap => cb(snap.val()));
-window._fbSetGho          = (data) => set(_fbGhoRef, data);
-window._fbOnValueGho      = (cb)   => onValue(_fbGhoRef, snap => cb(snap.val()));
+window._fbSet              = (data) => set(_fbRef,    data);
+window._fbOnValue          = (cb)   => onValue(_fbRef,    snap => cb(snap.val()));
+window._fbSetResources     = (data) => set(_fbResRef, data);
+window._fbOnValueResources = (cb)   => onValue(_fbResRef, snap => cb(snap.val()));
+window._fbSetGho           = (data) => set(_fbGhoRef, data);
+window._fbOnValueGho       = (cb)   => onValue(_fbGhoRef, snap => cb(snap.val()));
+
+window._fbAuth         = _fbAuth;
+window._fbSignIn       = (email, pw) => signInWithEmailAndPassword(_fbAuth, email, pw);
+window._fbSignOut      = () => signOut(_fbAuth);
+window._fbOnAuthChange = (cb) => onAuthStateChanged(_fbAuth, cb);
