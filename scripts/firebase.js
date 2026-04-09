1fffe1f3b9932e1cde8c7848034d527e88dda1e3
niighty-ai <gaeldu81@gmail.com>
2026-04-09 09:16:44 +0200

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
-import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged }
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDLpk2xBs-R9gM-tvKZ5abG-EaGNqKUGLk",
@@ -12,12 +13,13 @@ const firebaseConfig = {
   appId: "1:1005597262191:web:86901f88aecbd9ed30689a"
 };
 
-const _fbApp    = initializeApp(firebaseConfig);
-const _fbDb     = getDatabase(_fbApp);
+const _fbApp  = initializeApp(firebaseConfig);
+const _fbDb   = getDatabase(_fbApp);
+const _fbAuth = getAuth(_fbApp);
+
 const _fbRef    = ref(_fbDb, 'gantt_portfolio');
 const _fbResRef = ref(_fbDb, 'gantt_resources');
 const _fbGhoRef = ref(_fbDb, 'gantt_gho');
-const _fbAuth   = getAuth(_fbApp);
 
 window._fbSet              = (data) => set(_fbRef,    data);
 window._fbOnValue          = (cb)   => onValue(_fbRef,    snap => cb(snap.val()));
